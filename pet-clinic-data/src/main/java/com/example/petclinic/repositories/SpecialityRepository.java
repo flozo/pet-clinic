beb71bf002e67886e80546353ac4ceaa536d5889
flozo.home <github.mail@flozo.de>
2022-11-06 18:01:56 +0100

    Add repositories (closes #38)

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/repositories/SpecialityRepository.java
@@ -0,0 +1,8 @@
+package com.example.petclinic.repositories;
+
+import com.example.petclinic.model.Speciality;
+import org.springframework.data.repository.CrudRepository;
+
+public interface SpecialityRepository extends CrudRepository<Speciality, Long> {
+
+}
